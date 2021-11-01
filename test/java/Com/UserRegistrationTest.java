ce8233fa64767b7237fceaff15f7a6a07ba69d7f
Smruti <lenkasmruti123@gmail.com>
2021-11-01 23:26:20 +0530

    Added: Uc9_MoodAnalyser

--- test/java/Com/UserRegistrationTest.java
+++ test/java/Com/UserRegistrationTest.java
@@ -4,9 +4,44 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
+    public void givenFirstNameWhenShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean userTest = userRegistration.firstName("Smreu");
+        Assert.assertTrue(userTest);
+    }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean userTest = userRegistration.lastName("lenka");
+        Assert.assertTrue(userTest);
+    }
+
+    @Test
+    public void givenPassword_WhenSpecialCharacter_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean userTest = userRegistration.passwordSpecialCharacter("SmrutiLlnka@77");
+        Assert.assertTrue(userTest);
+    }
+
+    @Test
     public void givenAnyEmail_WhenValidOrNonValid_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean userTest = userRegistration.emailTest("abc+200@gmail.com");
+        boolean userTest = userRegistration.emailTest("lenkasmruti23@gmail.com");
         Assert.assertTrue(userTest);
+    }
+
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad() {
+        MoodAnalyser mood = new MoodAnalyser();
+        String moodTest = mood.analyseMoodHappyOrSad(" Sad message");
+        Assert.assertEquals("SAD", moodTest);
+    }
+
+    @Test
+    public void givenMessage_WhenNotSad_ShouldReturnHappy() {
+        MoodAnalyser mood = new MoodAnalyser();
+        String moodTest = mood.analyseMoodHappyOrSad(" Happy message");
+        Assert.assertEquals("Sad", moodTest);
     }
 }
