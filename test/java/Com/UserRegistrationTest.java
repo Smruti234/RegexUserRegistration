ce316ee461880fdfd9b33bdd545395dd796f0cf4
Smruti <lenkasmruti123@gmail.com>
2021-11-01 22:16:53 +0530

    Added: Uc3_Email

--- test/java/Com/UserRegistrationTest.java
+++ test/java/Com/UserRegistrationTest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
-    public void givenLastName_WhenProper_ShouldReturnTrue() {
+    public void givenEmail_WhenValid_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean userTest = userRegistration.lastName("Lenka");
+        boolean userTest = userRegistration.email("lenkasmruti.xyz@slen.com.in");
         Assert.assertTrue(userTest);
     }
 }
