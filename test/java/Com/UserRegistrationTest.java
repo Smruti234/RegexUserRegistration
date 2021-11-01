99db24c9c97bafb84e48f3be939a0c166ed0ac3b
Smruti <lenkasmruti123@gmail.com>
2021-11-02 00:04:33 +0530

    Added: Uc11_ParameterizedEmail

--- test/java/Com/UserRegistrationTest.java
+++ test/java/Com/UserRegistrationTest.java
@@ -1,47 +1,56 @@
 package Com;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 public class UserRegistrationTest {
+    private UserRegistration registration;
+    private String emailPattern;
+    private static boolean expectedResult;
+
+    public UserRegistrationTest(String emailPattern, boolean expectedResult) {
+        this.emailPattern = emailPattern;
+        this.expectedResult = expectedResult;
+    }
+
+    @Before
+    public void initialize() {
+        registration = new UserRegistration();
+    }
+
+    @Parameterized.Parameters
+    public static Collection emails() {
+        return Arrays.asList(new Object[][]{
+                        {"abc-100@yahoo.com", true},
+                        {"abc.100@yahoo.com", true},
+                        {"abc111@gmail.com", true},
+                        {"abc-100@abc.com", true},
+                        {"abc-100@abc.net", true},
+                        {"abc.100@abc.com.au", true},
+                        {"abc@gmail.com.com", true},
+                        {"abc@1.com", true},
+                        {"abc+100@gmail.com", true},
+                        {"abc", false},
+                        {".abc+100@gmail.com", false},
+                        {"abc+100@gmail.com", false},
+                        {"abc+100@gmail.a", false},
+                        {"abc+100@.gmail.com", false},
+                        {"abc+100*()@gmail.com", false},
+                        {"abc@abc.com", false}
+                }
+        );
+    }
+
     @Test
-    public void givenFirstNameWhenShouldReturnTrue() {
+    public void givenEmailAsVariable_ShouldReturnAsPerTheParameterizedResult() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean userTest = userRegistration.firstName("Smreu");
-        Assert.assertTrue(userTest);
-    }
-
-    @Test
-    public void givenLastName_WhenProper_ShouldReturnTrue() {
-        UserRegistration userRegistration = new UserRegistration();
-        boolean userTest = userRegistration.lastName("lenka");
-        Assert.assertTrue(userTest);
-    }
-
-    @Test
-    public void givenPassword_WhenSpecialCharacter_ShouldReturnTrue() {
-        UserRegistration userRegistration = new UserRegistration();
-        boolean userTest = userRegistration.passwordSpecialCharacter("SmrutiLlnka@77");
-        Assert.assertTrue(userTest);
-    }
-
-    @Test
-    public void givenAnyEmail_WhenValidOrNonValid_ShouldReturnTrue() {
-        UserRegistration userRegistration = new UserRegistration();
-        boolean userTest = userRegistration.emailTest("lenkasmruti23@gmail.com");
-        Assert.assertTrue(userTest);
-    }
-
-    @Test
-    public void givenMessage_WhenSad_ShouldReturnSad() {
-        MoodAnalyser mood = new MoodAnalyser();
-        String moodTest = mood.analyseMoodHappyOrSad(" Sad message");
-        Assert.assertEquals("SAD", moodTest);
-    }
-
-    @Test
-    public void givenMessage_WhenNotSad_ShouldReturnHappy() {
-        MoodAnalyser mood = new MoodAnalyser();
-        String moodTest = mood.analyseMoodHappyOrSad(" Happy message");
-        Assert.assertEquals("Sad", moodTest);
+        boolean userTest = userRegistration.emailTest(emailPattern, expectedResult);
+        System.out.println("sum of numbers = : " + userTest);
+        Assert.assertEquals(expectedResult,userTest);
     }
 }
